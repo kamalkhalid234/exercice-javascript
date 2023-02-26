5116559affd2f3eee7f89adb497357e27a53705e
khalid--rar <99455061+kamalkhalid234@users.noreply.github.com>
2023-02-26 16:13:36 +0100

    Add files via upload

---
+++ ww.js
@@ -0,0 +1,54 @@
+    
+/* -------------------------------------background------------------------------------*/
+         function col1() {
+                 var uCol = document.getElementById("txt");
+                 uCol.style.background="rgb(255, 0, 217)";
+        }
+         function col2() {
+                 var uCol = document.getElementById("txt");
+                 uCol.style.background=" rgb(170, 55, 185)";
+        }
+         function col3() {
+                 var uCol = document.getElementById("txt");
+                 uCol.style.background="rgb(52, 145, 220)";
+        }
+         function col4() {
+                 var uCol = document.getElementById("txt");
+                 uCol.style.background="rgb(235, 144, 18)";
+        }
+
+
+
+ /* -------------------------------------color de text------------------------------------*/         
+         document.getElementById('change').onclick = changecolor;
+             function col21(){
+                     document.body.style.color = "rgb(255, 0, 0)";
+            }
+             function col22(){
+                     document.body.style.color = "rgb(70, 207, 223)";
+            }
+             function col23(){
+                     document.body.style.color = "rgb(25, 0, 255)";
+            }
+             function col24(){
+                     document.body.style.color = "rgb(10, 76, 7)";
+            }
+         
+         
+/* -------------------------------------color de border------------------------------------*/
+             function col31(){
+                     const object = document.getElementById('txt')
+                     object.style.borderColor = "rgb(126, 239, 51)";
+            }
+             function col32(){
+                     const object = document.getElementById('txt')
+                     object.style.borderColor = "rgb(124, 68, 9)";
+            }
+             function col33(){
+                     const object = document.getElementById('txt')
+                     object.style.borderColor = "rgb(129, 123, 181)";
+            }
+             function col34(){
+                     const object = document.getElementById('txt')
+                     object.style.borderColor = "rgb(198, 236, 9)";
+             }
